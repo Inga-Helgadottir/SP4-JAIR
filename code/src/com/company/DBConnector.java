2a32d2439b634f22859931ebb1fa6bd8b3c65380
Inga-Helgadottitr <78418864+Inga-Helgadottitr@users.noreply.github.com>
2021-04-27 13:11:47 +0200

    changes

--- code/src/com/company/DBConnector.java
+++ code/src/com/company/DBConnector.java
@@ -1,47 +1,123 @@
 package com.company;
+import java.sql.*;
 
-public class DBConnector implements IO {
+public class DBConnector implements IO
+{
 
    // JDBC driver name and database URL
-   static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-   static final String DB_URL = "jdbc:mysql://localhost/tournament_manager";
+   //static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+   //static final String DB_URL = "jdbc:mysql://localhost/tournament_manager";
 
    //  Database credentials
-   static final String USER = "root";
-   static final String PASS = "kisshu25";
+   //static final String USER = "root";
+   //static final String PASS = "Password";
+
+   private String driverName = "com.mysql.jdbc.Driver";
+   private PreparedStatement pstmt = null;
+   private Connection connection;
+
+   public void DBBasicMethodSqlImplement()
+   {
+      try
+      //connection info
+      {
+         this.connection = DriverManager.getConnection("jdbc:mysql://localhost/tournament_manager", "root", "Password");
+      }
+      catch (SQLException throwables)
+      {
+         throwables.printStackTrace();
+      }
+   }
+
 
    @Override
-   public void readTournamentData(String path) {
+   public void readTournamentData(String path)
+   {
+      try
+      {
+         String sql = "SELECT * FROM tournaments";
+
+         pstmt = this.connection.prepareStatement(sql);
+         ResultSet rs = pstmt.executeQuery();
+
+         while (rs.next())
+         //prints all tournaments data
+         {
+            System.out.println("ID: " + rs.getInt("id") + ", tournament: " + rs.getString("tournament_name") + ", sport: " + rs.getString("sport") + ", mode: " + rs.getString("tournament_mode") + ", deadline: " + rs.getDate("sign_up_deadline"));
+         }
+         rs.close();
+      }
+
+      catch (SQLException throwables)
+      {
+         throwables.printStackTrace();
+      }
+   }
+   @Override
+   public void readGameDateData(String path, Tournament tournament)
+   {
+      try
+      {
+         String sql = "SELECT * FROM game_dates";
+
+         pstmt = this.connection.prepareStatement(sql);
+         ResultSet rs = pstmt.executeQuery();
+
+         while (rs.next())
+         //prints all game_dates data
+         {
+            System.out.println("ID: " + rs.getInt("id") + ", game date: "+ rs.getDate("game_date"));
+         }
+         rs.close();
+      }
+
+      catch (SQLException throwables)
+      {
+         throwables.printStackTrace();
+      }
+   }
+
+   @Override
+   public void readTeamData(String path, Tournament tournament)
+   {
+      try
+      {
+         String sql = "SELECT * FROM teams";
+
+         pstmt = this.connection.prepareStatement(sql);
+         ResultSet rs = pstmt.executeQuery();
+
+         while (rs.next())
+         //prints all teams data
+         {
+            System.out.println("ID: " + rs.getInt("id") + ", team name: " + rs.getString("team_name") + ", still in tournament?: " + rs.getInt("still_in_tournament") + ", point: " + rs.getInt("points") + ", goals made: " + rs.getInt("goals_made") + ", goals let int" + rs.getInt("goals_let_through") + "tournament id: " + rs.getInt("fk_tournament_id"));
+         }
+         rs.close();
+      }
+
+      catch (SQLException throwables)
+      {
+         throwables.printStackTrace();
+      }
+   }
+
+   @Override
+   public void saveTournamentData(String path, Tournament tournament) {
 
    }
 
    @Override
-   public void readGameDateData(String path, Tournament tournament) {
+   public void saveGameDateData(String path, Tournament tournament, String date) {
 
    }
 
    @Override
-   public void readTeamData(String path, Tournament tournament) {
+   public void saveTeamData(String path, Team team, Tournament tournament) {
 
    }
 
    @Override
-   public void saveTournamentData(String path, Tournament tournament){
-
-   }
-
-   @Override
-   public void saveGameDateData(String path, Tournament tournament, String date){
-
-   }
-
-   @Override
-   public void saveTeamData(String path, Team team, Tournament tournament){
-
-   }
-
-   @Override
-   public void deleteTournamentData(String path, Tournament tournament){
+   public void deleteTournamentData(String path, Tournament tournament) {
 
    }
 
@@ -51,11 +127,13 @@ public class DBConnector implements IO {
    }
 
    @Override
-   public void saveMatches(Match data){
+   public void saveMatches(Match data) {
 
    }
 
-   public void showMatchMenu(String matchType){
+   @Override
+   public void showMatchMenu(String matchType) {
 
    }
+
 }
